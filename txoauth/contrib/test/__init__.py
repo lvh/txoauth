474e9e22a8b4bf79666de996cbdc735c9c3bcaeb
Laurens Van Houtven <_@lvh.cc>
2010-09-02 14:02:22 +0200

    Fixed tests

--- txoauth/contrib/test/__init__.py
+++ txoauth/contrib/test/__init__.py
@@ -1,3 +1,40 @@
 """
 Tests for contributed txOAuth code.
 """
+from txoauth.authserver import interfaces
+from txoauth.contrib.simple import SimpleCallbackURLFactory
+from txoauth.test.test_authserver import (IDENTIFIER, BOGUS_IDENTIFIER, URL,
+                                          urlFactory)
+
+from twisted.trial.unittest import TestCase
+
+
+class SimpleCallbackURLFactoryTestCase(TestCase):
+    def setUp(self):
+        self.empty = SimpleCallbackURLFactory()
+        self.withURLs = SimpleCallbackURLFactory(spam="eggs")
+
+
+    def test_interface(self):
+        self.assertTrue(interfaces.ICallbackURLFactory
+                        .implementedBy(SimpleCallbackURLFactory))
+
+
+    def _genericFactoryTest(self, factory, identifier, expectedURL):
+        d = factory.get(identifier)
+        @d.addCallback
+        def cb(url):
+            self.assertEquals(url, expectedURL)
+        return d
+
+
+    def test_empty(self):
+        self._genericFactoryTest(self.empty, IDENTIFIER, None)
+
+
+    def test_registeredURL(self):
+        self._genericFactoryTest(urlFactory, IDENTIFIER, URL)
+
+
+    def test_missingURL(self):
+        self._genericFactoryTest(urlFactory, BOGUS_IDENTIFIER, None)
