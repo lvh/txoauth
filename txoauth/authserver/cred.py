6b2273d6dffb673d149c060025f364fd269b3e03
Laurens Van Houtven <_@lvh.cc>
2010-09-02 13:51:49 +0200

    Removed SimpleCallbackURLFactory from cred

--- txoauth/authserver/cred.py
+++ txoauth/authserver/cred.py
@@ -43,29 +43,6 @@ class Client(object):
 
 
 
-class SimpleCallbackURLFactory(object):
-    """
-    A simplistic, in-memory callback URL factory.
-
-    This is a wrapper around a dictionary.
-    """
-    implements(ICallbackURLFactory)
-
-    def __init__(self, **callbackURLs):
-        """
-        Initializes a SimpleCallbackURLFactory.
-
-        TODO: finish docstring
-        """
-        self._urls = callbackURLs
-
-
-    def get(self, clientIdentifier):
-        url = self._urls.get(clientIdentifier)
-        return defer.succeed(url)
-
-
-
 class ClientRealm(object):
     """
     A realm that produces clients.
