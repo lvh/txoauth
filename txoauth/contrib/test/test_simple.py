6225b16e4824150e45445421c02e851616c4d5d7
Laurens Van Houtven <_@lvh.cc>
2010-09-12 14:58:31 +0200

    More tests

--- txoauth/contrib/test/test_simple.py
+++ txoauth/contrib/test/test_simple.py
@@ -1,9 +1,11 @@
 """
 Tests for simple txOAuth implementations.
 """
-from txoauth import interfaces
+from txoauth import interfaces, token
 from txoauth.contrib import simple
 from txoauth.test.test_clientcred import IDENTIFIER, BOGUS_IDENTIFIER, URI
+from txoauth.test.test_token import TYPE, ASSERTION
+from txoauth.test.test_token import BOGUS_TYPE, BOGUS_ASSERTION
 
 from twisted.trial.unittest import TestCase
 
@@ -42,9 +44,73 @@ class SimpleRedirectURIFactoryTestCase(TestCase):
 
 class SimpleAssertionStoreTestCase(TestCase):
     def setUp(self):
+        self.assertion = token.Assertion(TYPE, ASSERTION)
+        self.bogusAssertion = token.Assertion(BOGUS_TYPE, BOGUS_ASSERTION)
+
         self.store = simple.SimpleAssertionStore()
-        self.noInvalidationStore = simple.SimpleAssertionStore
+        self.store.addAssertion(self.assertion)
+
+        self.store2 = simple.SimpleAssertionStore(forceInvalidation=False)
+        self.store2.addAssertion(self.assertion)
+
 
     def test_interface(self):
         self.assertTrue(interfaces.IAssertionStore
                         .implementedBy(simple.SimpleAssertionStore))
+
+
+    def _test_simple(self, store):
+        d = store.checkAssertion(self.assertion)
+
+        @d.addCallback
+        def cb(_):
+            return store.checkAssertion(self.assertion)
+
+        @d.addErrback
+        def eb(failure):
+            failure.trap("MISSING") # TODO: exceptions
+
+        return d
+
+
+    def test_simple(self):
+        return self._test_simple(self.store)
+
+
+    def test_simple_noForceInvalidation(self):
+        return self._test_simple(self.store2)
+
+
+    def _test_missing(self, store):
+        d = store.checkAssertion(self.bogusAssertion)
+
+        @d.addErrback
+        def eb(failure):
+            failure.trap("MISSING") # TODO: exceptions
+
+
+    def test_missing(self):
+        self._test_missing(self.store)
+
+
+    def test_missing_noForceInvalidation(self):
+        self._test_missing(self.store2)
+
+
+    def test_noInvalidation(self):
+        store = self.store2
+        def getAssertion():
+            return store.checkAssertion(self.assertion, invalidate=False)
+
+        d = getAssertion()
+        @d.addCallback
+        def tryAgain(_):
+            return getAssertion()
+
+
+    def test_enforcedInvalidation(self):
+        d = self.store.checkAssertion(self.assertion, invalidate=False)
+
+        @d.addErrback
+        def eb(failure):
+            failure.trap("MUST_INVALIDATE") # TODO: exceptions
