33cdfe2a2f87de2a4d282572ea7db6f19d28a862
Laurens Van Houtven <_@lvh.cc>
2010-09-17 22:52:19 +0200

    More tests\!

--- txoauth/test/test_twisted.py
+++ txoauth/test/test_twisted.py
@@ -13,7 +13,23 @@ class Hashable(FancyHashMixin):
 
 
 
+class DifferentHashable(FancyHashMixin):
+    compareAttributes = hashAttributes = ("value",)
+    def __init__(self, value):
+        self.value = value
+
+
+
 class PartialHashable(FancyHashMixin):
+    compareAttributes = ("valueOne", "valueTwo")
+    hashAttributes = ("valueOne",)
+
+    def __init__(self, valueOne, valueTwo):
+        self.valueOne, self.valueTwo = valueOne, valueTwo
+
+
+
+class DifferentPartialHashable(FancyHashMixin):
     compareAttributes = ("valueOne", "valueTwo")
     hashAttributes = ("valueOne",)
 
@@ -25,32 +41,42 @@ class PartialHashable(FancyHashMixin):
 class FancyHashMixinTests(TestCase):
     def test_equality(self):
         h1, h2 = Hashable(1), Hashable(1)
-
         self.assertEqual(h1, h1)
         self.assertEqual(h1, h2)
 
 
     def test_inequality(self):
         h1, h2 = Hashable(1), Hashable(2)
+        self.assertNotEqual(h1, h2)
 
+
+    def test_inequality_differentClasses(self):
+        h1, h2 = Hashable(1), DifferentHashable(2)
         self.assertNotEqual(h1, h2)
 
 
     def test_hashConsistent(self):
         h = Hashable(1)
-
         self.assertEqual(hash(h), hash(h))
 
 
     def test_hashEqual(self):
         h1, h2 = Hashable(1), Hashable(1)
+        self.assertEqual(hash(h1), hash(h2))
 
+
+    def test_hashEqual_differentClasses(self):
+        h1, h2 = Hashable(1), DifferentHashable(1)
         self.assertEqual(hash(h1), hash(h2))
 
 
     def test_hashNotEqual(self):
         h1, h2 = Hashable(1), Hashable(2)
+        self.assertNotEqual(hash(h1), hash(h2))
 
+
+    def test_hashNotEqual_differentClasses(self):
+        h1, h2 = Hashable(1), DifferentHashable(2)
         self.assertNotEqual(hash(h1), hash(h2))
 
 
@@ -63,38 +89,47 @@ class FancyHashMixinTests(TestCase):
 class PartialHashableTests(TestCase):
     def test_equality(self):
         h1, h2 = PartialHashable(1, 2), PartialHashable(1, 2)
-
         self.assertEqual(h1, h1)
         self.assertEqual(h1, h2)
 
 
     def test_inequality(self):
         h1, h2 = PartialHashable(1, 2), PartialHashable(2, 1)
-
         self.assertNotEqual(h1, h2)
 
 
     def test_hashConsistent(self):
         h = PartialHashable(1, 2)
-
         self.assertEqual(hash(h), hash(h))
 
 
     def test_hashEqual(self):
         h1, h2 = PartialHashable(1, 2), PartialHashable(1, 2)
+        self.assertEqual(hash(h1), hash(h2))
 
+
+    def test_hashEqual_differentClasses(self):
+        h1, h2 = PartialHashable(1, 2), DifferentPartialHashable(1, 2)
         self.assertEqual(hash(h1), hash(h2))
 
 
     def test_hashEqual_partial(self):
         h1, h2 = PartialHashable(1, 1), PartialHashable(1, 2)
+        self.assertEqual(hash(h1), hash(h2))
 
+
+    def test_hashEqual_partial_differentClasses(self):
+        h1, h2 = PartialHashable(1, 1), DifferentPartialHashable(1, 2)
         self.assertEqual(hash(h1), hash(h2))
 
 
     def test_hashNotEqual(self):
         h1, h2 = PartialHashable(1, 2), PartialHashable(2, 1)
+        self.assertNotEqual(hash(h1), hash(h2))
 
+
+    def test_hashNotEqual_differentClasses(self):
+        h1, h2 = PartialHashable(1, 2), PartialHashable(2, 1)
         self.assertNotEqual(hash(h1), hash(h2))
 
 
@@ -105,4 +140,9 @@ class PartialHashableTests(TestCase):
 
     def test_set_equalHashes(self):
         s = set([PartialHashable(1, 1), PartialHashable(1, 2)])
+        self.assertEqual(len(s), 2)
+
+
+    def test_set_equalHashes_differentClasses(self):
+        s = set([PartialHashable(1, 2), DifferentPartialHashable(1, 2)])
         self.assertEqual(len(s), 2)
