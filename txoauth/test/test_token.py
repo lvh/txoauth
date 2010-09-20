6681760f52d02b84ce1205d41bcf12d691f8b482
Laurens Van Houtven <_@lvh.cc>
2010-09-20 20:22:49 +0200

    Test refactoring and fixes

--- txoauth/test/test_token.py
+++ txoauth/test/test_token.py
@@ -6,14 +6,46 @@ from txoauth.test.test_clientcred import IDENTIFIER, BOGUS_IDENTIFIER, URI
 
 from twisted.trial.unittest import TestCase
 
+
+class _TokenRequestTests(TestCase):
+    interface, implementer = None, None
+
+    def setUp(self):
+        self.credentials = cred.ClientIdentifier(IDENTIFIER, URI)
+        self.bogusCredentials = cred.ClientIdentifier(BOGUS_IDENTIFIER, URI)
+
+
+    def test_interface(self):
+        self.assertTrue(token.ITokenRequest.implementedBy(self.implementer))
+        self.assertTrue(self.interface.implementedBy(self.implementer))
+
+
+    def test_clientCredentialsImmutability_same(self):
+        def mutateClient():
+            self.assertion.clientCredentials = self.credentials
+        self.assertRaises(AttributeError, mutateClient)
+
+
+    def test_clientCredentialsImmutability_different(self):
+        def mutateClient():
+            self.assertion.clientCredentials = self.bogusCredentials
+        self.assertRaises(AttributeError, mutateClient)
+
+
+
+class BaseTokenRequestTestCase(_TokenRequestTests):
+    interface, implementer = token.ITokenRequest, token._BaseTokenRequest
+
+
 TYPE, ASSERTION = "", ""
 BOGUS_TYPE, BOGUS_ASSERTION = "", ""
 
 
-class AssertionTests(TestCase):
+class AssertionTests(_TokenRequestTests):
+    interface, implementer = token.IAssertion, token.Assertion
+
     def setUp(self):
-        self.credentials = cred.ClientIdentifier(IDENTIFIER, URI)
-        self.bogusCredentials = cred.ClientIdentifier(BOGUS_IDENTIFIER, URI)
+        _TokenRequestTests.setUp(self)
         self.assertion = token.Assertion(self.credentials, TYPE, ASSERTION)
 
 
@@ -22,21 +54,9 @@ class AssertionTests(TestCase):
 
 
     def test_simple(self):
-        self.assertEqual(self.assertion.client, self.credentials)
+        self.assertEqual(self.assertion.clientCredentials, self.credentials)
         self.assertEqual(self.assertion.assertionType, TYPE)
         self.assertEqual(self.assertion.assertion, ASSERTION)
-
-
-    def test_clientImmutability_same(self):
-        def mutateClient():
-            self.assertion.client = self.credentials
-        self.assertRaises(AttributeError, mutateClient)
-
-
-    def test_clientImmutability_different(self):
-        def mutateClient():
-            self.assertion.client = self.bogusCredentials
-        self.assertRaises(AttributeError, mutateClient)
 
 
     def test_assertionTypeImmutability_same(self):
